59c18c4c7e3f1104cd3fb54c8b4c9b7a431dbf91
wangshaopeng <wangshaopeng@panda.tv>
2018-12-14 15:55:56 +0800

    添加一个默认add eslint-disable 的方法

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,15 @@
+/**
+ * @file 给src的js文件添加eslint-disable
+ **/
+const path = require('path');
 const prependFilterFile = require('../index');
 
-const folder = '../src';
 const addContent = '/* eslint-disable */ \n';
 const filterFun = (stat, path) => {
   return /\.js$/.test(path);
 };
 
-prependFilterFile(folder, addContent, filterFun);
+module.exports = function(handlerPath) {
+  const folder =  path.resolve(__dirname, handlerPath);
+  prependFilterFile(folder, addContent, filterFun);
+};
